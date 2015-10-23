70227ea0d357c51695a030ddca3f5a8b642e31bb
Kar Ho Lau <oredla@gmail.com>
2015-10-23 12:44:47 -0400

    updated footer bar.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,6 +3,6 @@
   <ol class="breadcrumb">
     <li><a href="/">home</a></li>
     <li class="active">@yield('title')</li>
-    <div style="float:right;"><a href="http://p1.orangeedward.xyz">Kar Ho Lau</a>, CSCI E-15, P3, &copy; {{ date('Y') }}</div>
+    <span style="float:right;"><a href="http://p1.orangeedward.xyz">Kar Ho Lau</a>, CSCI E-15, P3, &copy; {{ date('Y') }}</span>
   </ol>
 @show
